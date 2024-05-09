400b39a3acd51f91737f975cb09ba24cb4380ee2
hamitLicina <hamitlicina@gmail.com>
2024-05-09 17:53:19 +0300

    Made significant enhancements to feed and profile components, including modifications to comment rendering, post fetching, and post interaction functionality

--- src/components/FeedPosts/PostFooter.jsx
+++ src/components/FeedPosts/PostFooter.jsx
@@ -1,62 +1,74 @@
 import { Box, Button, Flex, Input, InputGroup, InputRightElement, Text } from '@chakra-ui/react'
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { UnlikeLogo, NotificationsLogo, CommentLogo } from './../../assets/constants';
+import usePostComment from '../../hooks/usePostComment';
+import useAuthStore from '../../store/authStore';
+import useLikePost from '../../hooks/useLikePost';
+import { timeAgo } from '../../utils/timeAgo';
 
 
 
-const PostFooter = ({ username, isProfilePage }) => {
+const PostFooter = ({ post, isProfilePage, creatorProfile }) => {
 
-    const [liked, setLiked] = useState(false)
-    const [likes, setLikes] = useState(1903)
 
-    const handleLike = () => {
-        if (liked) {
-            setLiked(false)
-            setLikes(likes - 1)
-        } else {
-            setLiked(true)
-            setLikes(likes + 1)
-        }
+    const { isCommenting, handlePostComment } = usePostComment()
+    const [comment, setComment] = useState("")
+    const authUser = useAuthStore(state => state.user)
+    const commentRef = useRef(null)
+    const { handleLikePost, isLiked, likes } = useLikePost(post)
+
+    const handleSubmitComment = async () => {
+        await handlePostComment(post.id, comment)
+        setComment("")
     }
 
     return (
         <Box my={10} marginTop={"auto"} >
-            <Flex alignItems={"center"} gap={4} w={"full"} pt={1} mb={2} mt={5} >
-                <Box onClick={handleLike} cursor={"pointer"} fontSize={18} >
-                    {!liked ? (<NotificationsLogo />) : (<UnlikeLogo />)}
+            <Flex alignItems={"center"} gap={4} w={"full"} pt={0} mb={2} mt={4} >
+                <Box onClick={handleLikePost} cursor={"pointer"} fontSize={18} >
+                    {!isLiked ? (<NotificationsLogo />) : (<UnlikeLogo />)}
                 </Box>
-                <Box cursor={"pointer"} fontSize={18}>
+                <Box cursor={"pointer"} fontSize={18} onClick={() => commentRef.current.focus()} >
                     <CommentLogo />
                 </Box>
             </Flex>
             <Text fontWeight={600} fontSize={"sm"} >
                 {likes} likes
             </Text>
+            {isProfilePage && (
+                <Text fontSize={12} color={"gray"} >
+                    Posted {timeAgo(post.createdAt)}
+                </Text>
+            )}
             {
                 !isProfilePage && (
                     <>
                         <Text fontSize={"sm"} fontWeight={700}>
-                            {username} {" "}
+                            {creatorProfile?.username} {" "}
                             <Text as="span" fontWeight={400}>
-                                What a nice smile, feeling good
+                                {post.caption}
                             </Text>
                         </Text>
                     </>
                 )
             }
-            <Text fontSize={"small"} color={"gray"}>
-                View all 2.222 comments
-            </Text>
-            <Flex alignItems={"center"} gap={2} justifyContent={"space-between"} w={"full"} >
-                <InputGroup>
-                    <Input variant={"flushed"} placeholder={"Add a comment ..."} fontSize={14} />
-                    <InputRightElement>
-                        <Button fontSize={14} color={"blue.500"} fontWeight={600} cursor={"pointer"} _hover={{ color: "white" }} bg={"transparent"} >
-                            Post
-                        </Button>
-                    </InputRightElement>
-                </InputGroup>
-            </Flex>
+            {post.comments.length > 0 && (
+                <Text fontSize={"small"} color={"gray"} cursor={"pointer"} >
+                    View all {post.comments.length} comments
+                </Text>
+            )}
+            {authUser && (
+                <Flex alignItems={"center"} gap={2} justifyContent={"space-between"} w={"full"} >
+                    <InputGroup>
+                        <Input variant={"flushed"} placeholder={"Add a comment ..."} fontSize={14} onChange={(e) => setComment(e.target.value)} value={comment} ref={commentRef} />
+                        <InputRightElement>
+                            <Button fontSize={14} color={"blue.500"} fontWeight={600} cursor={"pointer"} _hover={{ color: "white" }} bg={"transparent"} onClick={handleSubmitComment} isLoading={isCommenting} >
+                                Post
+                            </Button>
+                        </InputRightElement>
+                    </InputGroup>
+                </Flex>
+            )}
         </Box>
     )
 }
--- src/components/Profile/ProfilePost.jsx
+++ src/components/Profile/ProfilePost.jsx
@@ -1,15 +1,58 @@
-import { Avatar, Box, Divider, Flex, GridItem, Image, Modal, ModalBody, ModalCloseButton, ModalContent, ModalOverlay, Text, VStack, useDisclosure } from "@chakra-ui/react"
+import { Avatar, Button, Divider, Flex, GridItem, Image, Modal, ModalBody, ModalCloseButton, ModalContent, ModalOverlay, Text, VStack, useDisclosure } from "@chakra-ui/react"
 import { AiFillHeart } from "react-icons/ai"
 import { FaComment } from "react-icons/fa"
 import { MdDelete } from "react-icons/md"
 import Comment from "../Comment/Comment"
 import PostFooter from "../FeedPosts/PostFooter"
+import useUserProfileStore from "../../store/userProfileStore"
+import useAuthStore from './../../store/authStore';
+import useShowToast from "../../hooks/useShowToast"
+import { useState } from "react"
+import { firestore, storage } from "../../firebase/firebase"
+import { deleteObject, ref } from "firebase/storage"
+import { arrayRemove, deleteDoc, doc, updateDoc } from "firebase/firestore"
+import usePostStore from "../../store/postStore"
+import Caption from "../Comment/Caption"
 
 
 
-const ProfilePost = ({ img }) => {
+const ProfilePost = ({ post }) => {
 
     const { isOpen, onOpen, onClose } = useDisclosure()
+
+    const userProfile = useUserProfileStore((state) => state.userProfile)
+
+    const authUser = useAuthStore((state) => state.user)
+
+    const showToast = useShowToast()
+
+    const [isDeleting, setIsDeleting] = useState(false)
+
+    const deletePost = usePostStore((state) => state.deletePost)
+
+    const deletePostFromProfile = useUserProfileStore((state) => state.deletePost)
+
+    const handleDeletePost = async () => {
+        if (!window.confirm("Are you sure about you want to delete this post ?")) return;
+        if (isDeleting) return;
+
+        try {
+            imageRef = ref(storage, `posts/${post.id}`)
+            await deleteObject(imageRef)
+            const userRef = doc(firestore, "users", authUser.uid)
+            await deleteDoc(doc(firestore, "posts", post.id))
+            await updateDoc(userRef, {
+                posts: arrayRemove(post.id)
+            })
+            deletePost(post.id)
+            deletePostFromProfile(post.id)
+            showToast("Success", "Post Deleted Successfully", "success")
+        } catch (error) {
+            showToast("Error", error.message, "error")
+        } finally {
+            setIsDeleting(false)
+        }
+    }
 
 
     return (
@@ -20,18 +63,18 @@ const ProfilePost = ({ img }) => {
                         <Flex >
                             <AiFillHeart size={20} />
                             <Text fontWeight={"bold"} ml={2}>
-                                17
+                                {post.likes.length}
                             </Text>
                         </Flex>
                         <Flex>
                             <FaComment size={20} />
                             <Text fontWeight={"bold"} ml={2}>
-                                12
+                                {post.comments.length}
                             </Text>
                         </Flex>
                     </Flex>
                 </Flex>
-                <Image src={img} alt="profile post" w={"100%"} h={"100%"} objectFit={"cover"} />
+                <Image src={post.imageURL} alt="profile post" w={"100%"} h={"100%"} objectFit={"cover"} />
             </GridItem>
 
             <Modal isOpen={isOpen} onClose={onClose} isCentered={true} size={{ base: "3xl", md: "5xl" }} >
@@ -39,32 +82,35 @@ const ProfilePost = ({ img }) => {
                 <ModalContent>
                     <ModalCloseButton />
                     <ModalBody bg={"black"} pb={5} >
-                        <Flex gap="4" w={{ base: "90%", sm: "70%", md: "full" }} mx={"auto"} >
-                            <Box borderRadius={4} overflow={"hidden"} border={"1px solid"} borderColor={"whiteAlpha.300"} flex={1.5}>
-                                <Image src={img} alt="profile post" />
-                            </Box>
+                        <Flex gap="4" w={{ base: "90%", sm: "70%", md: "full" }} mx={"auto"} maxH={"90vh"} minH={"50vh"} >
+                            <Flex borderRadius={4} overflow={"hidden"} border={"1px solid"} borderColor={"whiteAlpha.300"} flex={1.5} justifyContent={"center"} alignItems={"center"} >
+                                <Image src={post.imageURL} alt="profile post" />
+                            </Flex>
                             <Flex flex={1} flexDir={"column"} px={10} display={{ base: "none", md: "flex" }} >
                                 <Flex alignItems={"center"} justifyContent={"space-between"}>
                                     <Flex alignItems={"center"} gap={4}>
-                                        <Avatar src="/profile.jpeg" size={"sm"} name="Hamit Licina" />
+                                        <Avatar src={userProfile.profilePicURL} size={"sm"} name={userProfile.username} />
                                         <Text fontWeight={"bold"} fontSize={12} >
-                                            Hamit Licina
+                                            {userProfile.username}
                                         </Text>
                                     </Flex>
-                                    <Box _hover={{ bg: "whiteAlpha.300", color: "red.600" }} borderRadius={4} p={1} >
-                                        <MdDelete size={20} cursor={"pointer"} />
-                                    </Box>
+                                    {authUser?.uid === userProfile.uid && (
+                                        <Button size={"sm"} bg={"transparent"} _hover={{ bg: "whiteAlpha.300", color: "red.600" }} borderRadius={4} p={1} onClick={handleDeletePost} isLoading={isDeleting} >
+                                            <MdDelete size={20} cursor={"pointer"} />
+                                        </Button>
+                                    )}
                                 </Flex>
                                 <Divider my={4} bg={"gray.500"} />
                                 <VStack w="full" alignItems={"start"} maxH={"350px"} overflowY={"auto"} >
-                                    <Comment createdAt="1 Day Ago" username="hamitlicina" profilePic="/profile.jpeg" text={"Dummy images from unsplash"} />
-
-                                    <Comment createdAt="12 Hours Ago" username="serralicina" profilePic="/img1.png" text={"Nice pic"} />
-
-                                    <Comment createdAt="3 Day Ago" username="kerimlicina" profilePic="/img2.png" text={"Good Clone"} />
+                                    {/* CAPTION */}
+                                    {post?.caption && <Caption post={post} />}
+                                    {/* COMMENTS */}
+                                    {post?.comments.map((comment) => (
+                                        <Comment key={comment?.id} comment={comment} />
+                                    ))}
                                 </VStack>
                                 <Divider my={4} bg={"gray.800"} />
-                                <PostFooter isProfilePage={true} />
+                                <PostFooter isProfilePage={true} post={post} />
                             </Flex>
                         </Flex>
                     </ModalBody>
